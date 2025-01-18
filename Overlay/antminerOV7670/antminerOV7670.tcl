7949682da5f924a7c615d07c78dc7c7c766e6ce2
Muhammad Yunus <muhammadyunusiot@gmail.com>
2025-01-19 00:27:08 +0700

    update

--- Overlay/antminerOV7670/antminerOV7670.tcl
+++ Overlay/antminerOV7670/antminerOV7670.tcl
@@ -70,22 +70,22 @@ set rc [catch {
   set_property board_part cholla.mmto.org:s9:part0:1.0 [current_project]
   set_property design_mode GateLvl [current_fileset]
   set_param project.singleFileAddWarning.threshold 0
-  set_property webtalk.parent_dir C:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_Project/OV7670-S9-DVI/OV7670-pynq.cache/wt [current_project]
-  set_property parent.project_path C:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_Project/OV7670-S9-DVI/OV7670-pynq.xpr [current_project]
+  set_property webtalk.parent_dir C:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_Project/AntminerCameraOV7670_V3/AntminerCameraOV7670_V3.cache/wt [current_project]
+  set_property parent.project_path C:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_Project/AntminerCameraOV7670_V3/AntminerCameraOV7670_V3.xpr [current_project]
   set_property ip_repo_paths {
   c:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_HLS_Project
   C:/Users/yunus/OneDrive/Documents/Xilinx/vivado-library-master
 } [current_project]
   update_ip_catalog
-  set_property ip_output_repo C:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_Project/OV7670-S9-DVI/OV7670-pynq.cache/ip [current_project]
+  set_property ip_output_repo C:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_Project/AntminerCameraOV7670_V3/AntminerCameraOV7670_V3.cache/ip [current_project]
   set_property ip_cache_permissions {read write} [current_project]
   set_property XPM_LIBRARIES {XPM_CDC XPM_FIFO XPM_MEMORY} [current_project]
-  add_files -quiet C:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_Project/OV7670-S9-DVI/OV7670-pynq.runs/synth_1/design_1_wrapper.dcp
+  add_files -quiet C:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_Project/AntminerCameraOV7670_V3/AntminerCameraOV7670_V3.runs/synth_1/design_1_wrapper.dcp
   set_msg_config -source 4 -id {BD 41-1661} -limit 0
   set_param project.isImplRun true
-  add_files C:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_Project/OV7670-S9-DVI/OV7670-pynq.srcs/sources_1/bd/design_1/design_1.bd
+  add_files C:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_Project/AntminerCameraOV7670_V3/AntminerCameraOV7670_V3.srcs/sources_1/bd/design_1/design_1.bd
   set_param project.isImplRun false
-  read_xdc C:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_Project/OV7670-S9-DVI/OV7670-pynq.srcs/constrs_1/new/io.xdc
+  read_xdc C:/Users/yunus/OneDrive/Documents/Xilinx/Vivado_Project/AntminerCameraOV7670_V3/AntminerCameraOV7670_V3.srcs/constrs_1/new/io.xdc
   set_param project.isImplRun true
   link_design -top design_1_wrapper -part xc7z010clg400-1
   set_param project.isImplRun false
@@ -106,7 +106,7 @@ set rc [catch {
   create_msg_db opt_design.pb
   opt_design 
   write_checkpoint -force design_1_wrapper_opt.dcp
-  create_report "impl_2_opt_report_drc_0" "report_drc -file design_1_wrapper_drc_opted.rpt -pb design_1_wrapper_drc_opted.pb -rpx design_1_wrapper_drc_opted.rpx"
+  create_report "impl_1_opt_report_drc_0" "report_drc -file design_1_wrapper_drc_opted.rpt -pb design_1_wrapper_drc_opted.pb -rpx design_1_wrapper_drc_opted.rpx"
   close_msg_db -file opt_design.pb
 } RESULT]
 if {$rc} {
@@ -126,9 +126,9 @@ set rc [catch {
   } 
   place_design 
   write_checkpoint -force design_1_wrapper_placed.dcp
-  create_report "impl_2_place_report_io_0" "report_io -file design_1_wrapper_io_placed.rpt"
-  create_report "impl_2_place_report_utilization_0" "report_utilization -file design_1_wrapper_utilization_placed.rpt -pb design_1_wrapper_utilization_placed.pb"
-  create_report "impl_2_place_report_control_sets_0" "report_control_sets -verbose -file design_1_wrapper_control_sets_placed.rpt"
+  create_report "impl_1_place_report_io_0" "report_io -file design_1_wrapper_io_placed.rpt"
+  create_report "impl_1_place_report_utilization_0" "report_utilization -file design_1_wrapper_utilization_placed.rpt -pb design_1_wrapper_utilization_placed.pb"
+  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file design_1_wrapper_control_sets_placed.rpt"
   close_msg_db -file place_design.pb
 } RESULT]
 if {$rc} {
@@ -145,14 +145,14 @@ set rc [catch {
   create_msg_db route_design.pb
   route_design 
   write_checkpoint -force design_1_wrapper_routed.dcp
-  create_report "impl_2_route_report_drc_0" "report_drc -file design_1_wrapper_drc_routed.rpt -pb design_1_wrapper_drc_routed.pb -rpx design_1_wrapper_drc_routed.rpx"
-  create_report "impl_2_route_report_methodology_0" "report_methodology -file design_1_wrapper_methodology_drc_routed.rpt -pb design_1_wrapper_methodology_drc_routed.pb -rpx design_1_wrapper_methodology_drc_routed.rpx"
-  create_report "impl_2_route_report_power_0" "report_power -file design_1_wrapper_power_routed.rpt -pb design_1_wrapper_power_summary_routed.pb -rpx design_1_wrapper_power_routed.rpx"
-  create_report "impl_2_route_report_route_status_0" "report_route_status -file design_1_wrapper_route_status.rpt -pb design_1_wrapper_route_status.pb"
-  create_report "impl_2_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file design_1_wrapper_timing_summary_routed.rpt -pb design_1_wrapper_timing_summary_routed.pb -rpx design_1_wrapper_timing_summary_routed.rpx -warn_on_violation "
-  create_report "impl_2_route_report_incremental_reuse_0" "report_incremental_reuse -file design_1_wrapper_incremental_reuse_routed.rpt"
-  create_report "impl_2_route_report_clock_utilization_0" "report_clock_utilization -file design_1_wrapper_clock_utilization_routed.rpt"
-  create_report "impl_2_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file design_1_wrapper_bus_skew_routed.rpt -pb design_1_wrapper_bus_skew_routed.pb -rpx design_1_wrapper_bus_skew_routed.rpx"
+  create_report "impl_1_route_report_drc_0" "report_drc -file design_1_wrapper_drc_routed.rpt -pb design_1_wrapper_drc_routed.pb -rpx design_1_wrapper_drc_routed.rpx"
+  create_report "impl_1_route_report_methodology_0" "report_methodology -file design_1_wrapper_methodology_drc_routed.rpt -pb design_1_wrapper_methodology_drc_routed.pb -rpx design_1_wrapper_methodology_drc_routed.rpx"
+  create_report "impl_1_route_report_power_0" "report_power -file design_1_wrapper_power_routed.rpt -pb design_1_wrapper_power_summary_routed.pb -rpx design_1_wrapper_power_routed.rpx"
+  create_report "impl_1_route_report_route_status_0" "report_route_status -file design_1_wrapper_route_status.rpt -pb design_1_wrapper_route_status.pb"
+  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file design_1_wrapper_timing_summary_routed.rpt -pb design_1_wrapper_timing_summary_routed.pb -rpx design_1_wrapper_timing_summary_routed.rpx -warn_on_violation "
+  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file design_1_wrapper_incremental_reuse_routed.rpt"
+  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file design_1_wrapper_clock_utilization_routed.rpt"
+  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file design_1_wrapper_bus_skew_routed.rpt -pb design_1_wrapper_bus_skew_routed.pb -rpx design_1_wrapper_bus_skew_routed.rpx"
   close_msg_db -file route_design.pb
 } RESULT]
 if {$rc} {
